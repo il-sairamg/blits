8e54aa82902459626b9f6bda3b5331e0780603f4
Michiel van der Geest <hello@michielvandergeest.com>
2026-02-26 09:15:56 +0100

    Added default value for platform launch setting and added type definitions.

--- src/engines/L3/launch.js
+++ src/engines/L3/launch.js
@@ -100,7 +100,7 @@ export default (App, target, settings = {}) => {
         textureMemory: textureMemorySettings(settings),
         createImageBitmapSupport: 'auto',
         targetFPS: 'maxFPS' in settings ? settings.maxFPS : 0,
-        platform: settings.platform,
+        platform: 'platform' in settings ? settings.platform : null,
       },
       ...(settings.advanced || {}),
     },
